8daf885ad8ae60ba18cca8565134371cc8a0901c
nojiritakeshi <takesoccer08nine@ezweb.ne.jp>
2023-12-30 19:39:56 +0900

    auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@sidebase/nuxt-auth"],
+  build: {
+    transpile: ["jsonwebtoken"],
+  },
+  auth: {
+    // baseURL: "/",
+    provider: {
+      type: "local",
+      endpoints: {
+        getSession: { path: "/user" },
+      },
+      pages: {
+        login: "/",
+      },
+      token: {
+        signInResponseTokenPointer: "/token/accessToken",
+      },
+      sessionDataType: {
+        id: "string",
+        email: "string",
+        name: "string",
+        role: "admin | guest | account",
+        subscriptions: "{ id: number, status: 'ACTIVE' | 'INACTIVE' }[]",
+      },
+    },
+    session: {
+      // Whether to refresh the session every time the browser window is refocused.
+      enableRefreshOnWindowFocus: true,
+
+      // Whether to refresh the session every `X` milliseconds. Set this to `false` to turn it off. The session will only be refreshed if a session already exists.
+      enableRefreshPeriodically: 5000,
+    },
+    globalAppMiddleware: {
+      isEnabled: true,
+      addDefaultCallbackUrl: true,
+    },
+  },
+});
